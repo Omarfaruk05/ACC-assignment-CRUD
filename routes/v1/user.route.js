092b90f7f82ef2d37ba31cc62ad2a46b64ee40d8
Omarfaruk05 <mdomarfrauk149518.com>
2022-09-03 23:58:43 +0600

    added status

--- routes/v1/user.route.js
+++ routes/v1/user.route.js
@@ -6,108 +6,18 @@ let users;
 getUsers((data) =>{
     users = data;
 },(error)=>{
-    console.log(error);
+    
 }
 )
-
-let users1;
-
-// const users = [
-//     {
-//         id: 1,
-//         gander: 'Female',
-//         name: 'Farhana',
-//         contact: '+8801567900262',
-//         address:'Gazipur, Dhaka, Bangladesh',
-//         photoUrl: 'https://i.ibb.co/ySNYS6g/In-Shot-20220802-210749661.jpg',
-//     },
-//     {
-//         id: 2,
-//         gander: 'Male',
-//         name: 'Omar Faruk',
-//         contact: '+8801567900262',
-//         address:'Gazipur, Dhaka, Bangladesh',
-//         photoUrl: 'https://i.ibb.co/ySNYS6g/In-Shot-20220802-210749661.jpg',
-//     },
-//     {
-//         id: 3,
-//         gander: 'Male',
-//         name: 'Md. Omar Faruk',
-//         contact: '+8801567900262',
-//         address:'Gazipur, Dhaka, Bangladesh',
-//         photoUrl: 'https://i.ibb.co/ySNYS6g/In-Shot-20220802-210749661.jpg',
-//     },
-//     {
-//         id: 4,
-//         gander: 'Female',
-//         name: 'Kobita',
-//         contact: '+8801567900262',
-//         address:'Gazipur, Dhaka, Bangladesh',
-//         photoUrl: 'https://i.ibb.co/ySNYS6g/In-Shot-20220802-210749661.jpg',
-//     },
-//     {
-//         id: 5,
-//         gander: 'Female',
-//         name: 'Chionika',
-//         contact: '+8801567900262',
-//         address:'Gazipur, Dhaka, Bangladesh',
-//         photoUrl: 'https://i.ibb.co/ySNYS6g/In-Shot-20220802-210749661.jpg',
-//     },
-//     {
-//         id: 6,
-//         gander: 'Kale',
-//         name: 'Kabir Shing',
-//         contact: '+8801567900262',
-//         address:'Gazipur, Dhaka, Bangladesh',
-//         photoUrl: 'https://i.ibb.co/ySNYS6g/In-Shot-20220802-210749661.jpg',
-//     },
-//     {
-//         id: 7,
-//         gander: 'Male',
-//         name: 'Abdullah',
-//         contact: '+8801567900262',
-//         address:'Gazipur, Dhaka, Bangladesh',
-//         photoUrl: 'https://i.ibb.co/ySNYS6g/In-Shot-20220802-210749661.jpg',
-//     },
-//     {
-//         id: 8,
-//         gander: 'Male',
-//         name: 'Alif',
-//         contact: '+8801567900262',
-//         address:'Gazipur, Dhaka, Bangladesh',
-//         photoUrl: 'https://i.ibb.co/ySNYS6g/In-Shot-20220802-210749661.jpg',
-//     },
-//     {
-//         id: 9,
-//         gander: 'Male',
-//         name: 'Md. Rasel Hassan',
-//         contact: '+8801567900262',
-//         address:'Gazipur, Dhaka, Bangladesh',
-//         photoUrl: 'https://i.ibb.co/ySNYS6g/In-Shot-20220802-210749661.jpg',
-//     },
-//     {
-//         id: 10,
-//         gander: 'Female',
-//         name: 'Sabiha',
-//         contact: '+8801567900262',
-//         address:'Gazipur, Dhaka, Bangladesh',
-//         photoUrl: 'https://i.ibb.co/ySNYS6g/In-Shot-20220802-210749661.jpg',
-//     },
-//     {
-//         "id": 11,
-//         "gander": "Female",
-//         "name": "Kobita Akhter",
-//         "contact": "+8801567900262",
-//         "address": "Gazipur, Dhaka, Bangladesh",
-//         "photoUrl": "https://i.ibb.co/ySNYS6g/In-Shot-20220802-210749661.jpg"
-//     },
-// ];
 
 const router = express.Router();
 
 router.get('/all', (req, res) =>{
   const {limit} = req.query;
-  res.send(users.slice(0, limit));
+  res.status(200).send({
+    status: 'Success',
+    data:users.slice(0, limit)
+  });
 });
 
 router.get('/random', (req, res) =>{
@@ -115,7 +25,10 @@ router.get('/random', (req, res) =>{
 
   const randomIntegerNumber = Math.ceil(randomNumber);
   const randomUser = users.find(user => user.id === randomIntegerNumber)
-  res.send(randomUser);
+  res.status(200).send({
+    status:'Success',
+    data: randomUser,
+  });
 });
 
 router.post('/save', (req, res) => {
@@ -123,10 +36,15 @@ router.post('/save', (req, res) => {
     const {id, gander, contact, name, address, photoUrl} = newUser;
     if(gander && name && address && photoUrl && contact){
         users.push(newUser);
-        res.send(users);
+        res.status(200).send({
+            status:'Success',
+            data: users,
+          });
     }
     else{
-        res.send({message: 'Please input all the recomended value'})
+        res.status(403).send({
+            status: 'Forbiden',
+            message: 'Please input all the recomended value'})
     }
 
 });
@@ -143,10 +61,16 @@ router.patch('/update', (req, res) =>{
         updatedUser.contact = updatedInfo.contact?updatedInfo.contact : updatedUser.contact;
         updatedUser.photoUrl = updatedInfo.photoUrl?updatedInfo.photoUrl : updatedUser.photoUrl;
        
-        res.send(users)
+        res.status(200).send({
+            status:'Success',
+            data: users,
+          });
     }
     else{
-        res.send({message: 'Your id must be a number.'})
+        res.status(403).send({
+            status: 'Forbidden',
+            message: 'Your id must be a number.'
+        })
     }
     
     
@@ -174,7 +98,7 @@ router.patch('/bulk-update', (req, res) => {
         }
         res.status(201).send({
             status: 'Success',
-            data: {user: users},
+            data:  users,
         })
     }catch(error){
         res.status(400).send({
@@ -188,10 +112,16 @@ router.delete('/delete', (req, res) => {
     const {id} = req.body;
    if(Number(id)){
     const remainUsers = users.filter(user => user.id !== Number(id));
-    res.send(remainUsers);
+    res.status(200).send({
+        status:'Success',
+        data: remainUsers,
+      });
    }
    else{
-    res.send({message: 'Please input a valite id.'})
+    res.status(403).send({
+        status: 'Forbidden',
+        message: 'Please input a valite id.'
+    })
    }
 });
 
